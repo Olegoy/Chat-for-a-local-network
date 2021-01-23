cca43eae3635a26dc87c3e66a28638949bc26e61
Olegoy <73956020+Olegoy@users.noreply.github.com>
2021-01-23 15:23:50 +0300

    Add source file to chat in first time

---
+++ chat/ConsoleHelper.java
@@ -0,0 +1,37 @@
+﻿package chat;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ConsoleHelper {
+    private static BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void writeMessage(String message){
+        System.out.println(message);
+    }
+
+    public static String readString() {
+        String readMessage = null;
+        while (true) {
+            try {
+                readMessage = bufferedReader.readLine();
+           //     bufferedReader.close();  //  при запуске бота выдает ошибку
+                return readMessage;
+            } catch (IOException e) {
+                System.out.println("Произошла ошибка при попытке ввода текста. Попробуйте еще раз." );
+            }
+        }
+    }
+
+    public static int readInt(){
+        int number = 0;
+        try{
+            number = Integer.parseInt(readString());
+        }catch (NumberFormatException e) {
+            System.out.println("Не похоже на число!");
+            number = Integer.parseInt(readString());
+        }
+        return number;
+    }
+}
